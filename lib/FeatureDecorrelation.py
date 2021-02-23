103a52a24549a196ecb2dd002f94801f47198860
notfolder <notfolder@gmail.com>
2021-02-23 09:08:12 +0900

    fix

--- lib/FeatureDecorrelation.py
+++ lib/FeatureDecorrelation.py
@@ -9,19 +9,19 @@ class FeatureDecorrelation(nn.Module):
         self.low_dim = torch.tensor(low_dim)
         self.tau2 = torch.tensor(tau2)
         self.l2norm = Normalize(2)
-        self.seq = torch.tensor(torch.range(0, low_dim-1), dtype=torch.long).to(get_dev())
-        self.ce = torch.nn.CrossEntropyLoss()
+        #self.seq = torch.tensor(torch.range(0, low_dim-1), dtype=torch.long).to(get_dev())
+        #self.ce = torch.nn.CrossEntropyLoss()
 
     def forward(self, features):
         #Vt = torch.t(features)
         Vt = self.l2norm(torch.t(features))
-        #first = -torch.sum(torch.pow(Vt, 2), 1)/self.tau2
+        first = -torch.sum(torch.pow(Vt, 2), 1)/self.tau2
         inner_product = torch.mm(Vt, features)/self.tau2
         #print(inner_product.shape)
-        #second = torch.logsumexp(inner_product, 1)
+        second = torch.logsumexp(inner_product, 1)
         #Lf = torch.sum(first + second)
-        #Lf = torch.mean(first + second)
-        Lf = self.ce(inner_product, self.seq)
+        Lf = torch.mean(first + second)
+        #Lf = self.ce(inner_product, self.seq)
         #old = self.forward_old(features)
         #print(old)
         #print(Lf)
